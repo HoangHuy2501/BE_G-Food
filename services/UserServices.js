dd18fd745ab6b0d780a85ce8932ac675a49a198e
HoangHuy2501 <huy04.developer@gmail.com>
2025-12-04 16:24:55 +0700

    fix update user

--- services/UserServices.js
+++ services/UserServices.js
@@ -1,5 +1,6 @@
 
 const UserRepository=require('../repository/UserRepository');
+const AuthRepository = require("../repository/AuthRepository");
 const ApiErorr= require('../utils/ApiError');
 class UserServices{
     async getUserByID(id){
@@ -17,6 +18,14 @@ class UserServices{
         try {
             if(!id){
                 throw ApiErorr.ValidationError('ID is required');
+            }
+            const emailUser=await UserRepository.getUserByID(id)
+            // check nếu email body mà khác email trong db thi check xem email nay da ton tai chua, còn nếu email body === email trong db thi khong check
+            if(req.body.email !== emailUser.dataValues.email){
+                const checkMail=await AuthRepository.checkEmail(req.body.email)
+                if(checkMail){
+                    throw ApiErorr.ValidationError("Email already exists")
+                }
             }
             return await UserRepository.updateUser(id,req.body);
         } catch (error) {
